a9f421fc0e6ea342d372fab874832fae34c2730f
Alessandro <alembfilho@gmail.com>
2022-05-02 10:28:25 -0300

    widget

--- nlw-return/web/src/components/Widget.tsx
+++ nlw-return/web/src/components/Widget.tsx
@@ -1,3 +1,14 @@
+import { ChatTeardropDots } from 'phosphor-react'
+
 export function Widget() {
-  return <h1>Widget</h1>
+  return (
+    <div className="absolute right-5 bottom-5">
+      <button className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2 max-w-0">Feedback</span>
+        </span>
+      </button>
+    </div>
+  )
 }
