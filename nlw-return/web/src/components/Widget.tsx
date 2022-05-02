1e1ead8692f99172fa59b23099d9fdaa35a1fb5a
Alessandro <alembfilho@gmail.com>
2022-05-02 10:39:52 -0300

    popover

--- nlw-return/web/src/components/Widget.tsx
+++ nlw-return/web/src/components/Widget.tsx
@@ -1,14 +1,21 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
+
   return (
-    <div className="absolute right-5 bottom-5">
-      <button className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
+    <Popover className="absolute right-5 bottom-5">
+      <Popover.Panel>Hello</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+
+        <span
+          className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"
+        >
           <span className="pl-2 max-w-0">Feedback</span>
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
