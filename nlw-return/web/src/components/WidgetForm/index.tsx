2e1b14a25d62f72e2ca6120bcf4c078c84551a78
Alessandro <alembfilho@gmail.com>
2022-05-03 14:38:35 -0300

    split components

--- nlw-return/web/src/components/WidgetForm/index.tsx
+++ nlw-return/web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,11 @@
-import { CloseButton } from "./CloseButton";
-import bugImageUrl from '../images/bug.svg'
-import ideaImageUrl from '../images/idea.svg'
-import thoughtImageUrl from '../images/thought.svg'
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../images/bug.svg'
+import ideaImageUrl from '../../images/idea.svg'
+import thoughtImageUrl from '../../images/thought.svg'
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -28,7 +29,7 @@ const feedbackTypes = {
   }
 }
 
-type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
 
@@ -41,17 +42,7 @@ export function WidgetForm() {
       </header>
 
       {!feedbackType ? (
-        <div className="flex py-8 w-full gap-2">
-          {Object.entries(feedbackTypes).map(([key, value]) => <button
-            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-            type="button"
-            key={key}
-            onClick={() => setFeedbackType(key as FeedbackType)}
-          >
-            <img src={value.image.source} alt={value.image.alt} />
-            <span>{value.title}</span>
-          </button>)}
-        </div>
+        <FeedbackTypeStep setFeedbackType={setFeedbackType} />
       ) : (
         <p>Hello World</p>
       )}
