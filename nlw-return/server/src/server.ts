ff23fe9dd8c347de2455f046df39f67ef2942105
Alessandro <alembfilho@gmail.com>
2022-05-04 20:23:29 -0300

    email

--- nlw-return/server/src/server.ts
+++ nlw-return/server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma'
 import express from 'express'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2f8bfc5edcde70",
+    pass: "61dba4a1be53e5"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -16,8 +26,22 @@ app.post('/feedbacks', async (req, res) => {
     }
   })
 
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Alessandro Melgaço <alembfilho@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size:16px; color:#111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+
   return res.status(201).json({ data: feedback })
 })
+
+
 
 app.listen(3333, () => {
   console.log('Server running')
