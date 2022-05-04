b7107cd918b07e463af56bfbe4b077149fa1a0df
Alessandro <alembfilho@gmail.com>
2022-05-03 23:15:47 -0300

    bugfix width

--- nlw-return/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ nlw-return/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -42,7 +42,7 @@ export function FeedbackContentStep({ feedbackType, setFeedbackType, setFeedback
 
     <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
       <textarea
-        className="min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-700"
+        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-700"
         placeholder="Conte com detalhes o que está acontecendo..."
         onChange={e => setComment(e.target.value)}
       />
