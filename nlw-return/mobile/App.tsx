db617f242dcf1e4be76760aea37ad19c6117be33
Alessandro Melgaço <alembfilho@gmail.com>
2022-05-06 19:48:23 -0300

    bottomSheet

--- nlw-return/mobile/App.tsx
+++ nlw-return/mobile/App.tsx
@@ -1,10 +1,11 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,13 +22,14 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
 
       <StatusBar
         style='light'
         backgroundColor='transparent'
         translucent
       />
+
+      <Widget />
 
     </View>
   );
